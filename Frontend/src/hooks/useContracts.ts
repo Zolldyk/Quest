0730defdd5359fdfb0379493ea5238ab156b7c7d
Zoll <buadesoye@gmail.com>
2025-08-02 12:41:25 +0100

    Fix contract ABI format for thirdweb v5 compatibility
    - Convert StakingPool ABI from human-readable to full JSON format
    - Convert NFTMinter ABI to full JSON format with proper tuple definitions
    - Should resolve 'Could not resolve method' errors for getPoolBalance, getPoolStats, getStakerInfo, totalSupply

--- Frontend/src/hooks/useContracts.ts
+++ Frontend/src/hooks/useContracts.ts
@@ -5,25 +5,152 @@ import { toast } from "react-hot-toast";
 
 // ============ Contract ABIs ============
 const STAKING_POOL_ABI = [
-  "function stake(uint256 amount) external",
-  "function unstake(uint256 amount) external", 
-  "function getPoolBalance() external view returns (uint256)",
-  "function getStakerInfo(address staker) external view returns (uint256 stakedAmount, uint256 stakeTimestamp, uint256 lastUpdateTime, bool isActive)",
-  "function getPoolStats() external view returns (uint256 totalPoolBalance, uint256 totalStakers, uint256 totalRewardsDistributed, uint256 minimumStakeAmount)",
-  "function getStakingToken() external view returns (address)",
-  "function distributeReward(address recipient, uint256 amount) external",
-  "function replenishPool(uint256 amount) external",
-  "function getQuestManager() external view returns (address)",
-  "function getConfig() external view returns (address stakingToken, address questManager, uint256 minimumStakeAmount, uint256 emergencyWithdrawDelay, bool isPaused)",
+  {
+    "type": "function",
+    "name": "stake",
+    "inputs": [{"name": "amount", "type": "uint256"}],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "unstake", 
+    "inputs": [{"name": "amount", "type": "uint256"}],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "getPoolBalance",
+    "inputs": [],
+    "outputs": [{"name": "", "type": "uint256"}],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "getStakerInfo",
+    "inputs": [{"name": "staker", "type": "address"}],
+    "outputs": [
+      {"name": "stakedAmount", "type": "uint256"},
+      {"name": "stakeTimestamp", "type": "uint256"}, 
+      {"name": "lastUpdateTime", "type": "uint256"},
+      {"name": "isActive", "type": "bool"}
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "getPoolStats", 
+    "inputs": [],
+    "outputs": [
+      {"name": "totalPoolBalance", "type": "uint256"},
+      {"name": "totalStakers", "type": "uint256"},
+      {"name": "totalRewardsDistributed", "type": "uint256"},
+      {"name": "minimumStakeAmount", "type": "uint256"}
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "getStakingToken",
+    "inputs": [],
+    "outputs": [{"name": "", "type": "address"}],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "distributeReward",
+    "inputs": [
+      {"name": "recipient", "type": "address"},
+      {"name": "amount", "type": "uint256"}
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "replenishPool",
+    "inputs": [{"name": "amount", "type": "uint256"}],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "getQuestManager",
+    "inputs": [],
+    "outputs": [{"name": "", "type": "address"}],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "getConfig",
+    "inputs": [],
+    "outputs": [
+      {"name": "stakingToken", "type": "address"},
+      {"name": "questManager", "type": "address"},
+      {"name": "minimumStakeAmount", "type": "uint256"},
+      {"name": "emergencyWithdrawDelay", "type": "uint256"},
+      {"name": "isPaused", "type": "bool"}
+    ],
+    "stateMutability": "view"
+  }
 ] as const;
 
 const NFT_MINTER_ABI = [
-  "function totalSupply() external view returns (uint256)",
-  "function balanceOf(address owner) external view returns (uint256)",
-  "function tokenURI(uint256 tokenId) external view returns (string)",
-  "function getUserBadges(address user) external view returns (uint256[])",
-  "function getBadge(uint256 tokenId) external view returns (tuple(uint256 questId, address recipient, string tweetUrl, uint256 mintTime, uint256 questReward, string questTitle, bool isValid))",
-  "function getUserBadgeCount(address user) external view returns (uint256)",
+  {
+    "type": "function",
+    "name": "totalSupply",
+    "inputs": [],
+    "outputs": [{"name": "", "type": "uint256"}],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function", 
+    "name": "balanceOf",
+    "inputs": [{"name": "owner", "type": "address"}],
+    "outputs": [{"name": "", "type": "uint256"}],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "tokenURI", 
+    "inputs": [{"name": "tokenId", "type": "uint256"}],
+    "outputs": [{"name": "", "type": "string"}],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "getUserBadges",
+    "inputs": [{"name": "user", "type": "address"}],
+    "outputs": [{"name": "", "type": "uint256[]"}],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "getBadge",
+    "inputs": [{"name": "tokenId", "type": "uint256"}],
+    "outputs": [{
+      "name": "",
+      "type": "tuple",
+      "components": [
+        {"name": "questId", "type": "uint256"},
+        {"name": "recipient", "type": "address"},
+        {"name": "tweetUrl", "type": "string"},
+        {"name": "mintTime", "type": "uint256"},
+        {"name": "questReward", "type": "uint256"},
+        {"name": "questTitle", "type": "string"},
+        {"name": "isValid", "type": "bool"}
+      ]
+    }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "getUserBadgeCount",
+    "inputs": [{"name": "user", "type": "address"}],
+    "outputs": [{"name": "", "type": "uint256"}],
+    "stateMutability": "view"
+  }
 ] as const;
 
 const ERC20_ABI = [
